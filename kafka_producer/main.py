02a124445435f2ece75d84aa74e25f6d231d3785
Vladlen <vladlensakhnyuk@yandex.ru>
2026-06-03 22:48:07 +0300

    Added kafka and kafka producer to imitate continuous stream for stage 1 of the tutorial

---
+++ kafka_producer/main.py
@@ -0,0 +1,34 @@
+import logging
+
+from uuid import uuid1
+from pendulum import now
+from random import randint
+from json import dumps
+from time import sleep
+
+from kafka import KafkaProducer
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+
+    try:
+        producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: dumps(v).encode('utf-8'))
+        while True:
+            message = {
+                "event_id": str(uuid1()),
+                "event_time": now(tz="UTC").to_iso8601_string(),
+                "user_id": randint(10, 100),
+                "action": ["click", "view", "purchase"][randint(0, 2)],
+                "value": randint(1, 1000)
+            }
+
+            producer.send("stream-input", message)
+            print("Message sent!")
+
+            sleep(0.5)
+
+    except KeyboardInterrupt:
+        print("Shutting down producer...")
