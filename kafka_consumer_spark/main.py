3f3ffeb8b57a036b0a966cbd5942cba361bcc3ab
Vladlen <vladlensakhnyuk@yandex.ru>
2026-06-07 21:50:00 +0300

    Added data transforation and filtering

--- kafka_consumer_spark/main.py
+++ kafka_consumer_spark/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
@@ -17,13 +18,26 @@ if __name__ == '__main__':
             .format("kafka") \
             .option("kafka.bootstrap.servers", "kafka:9092") \
             .option("subscribe", "stream-input") \
+            .option("failOnDataLoss", "false") \
             .load()
 
+        # Define DDL
+        kafka_message_schema = "event_id string, event_time timestamp, user_id int, action string, value int"
+
         query = (df
-                 .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "topic", "partition", "offset",
-                             "timestamp")
+                 .select(from_json(col("value").cast("string"),
+                                   kafka_message_schema)
+                         # required alias for further selecting
+                         .alias("json_data"),
+                         "offset",
+                         col("timestamp").alias("processing_ts"),
+                         )
+                 .select("json_data.event_id", "json_data.user_id", "json_data.action", "json_data.value",
+                         col("json_data.event_time").alias("event_ts"), "offset", "processing_ts")
+                 .filter(col("action") == "click")
                  .writeStream
                  .trigger(processingTime="5 seconds")
+                 # Output result to console
                  .format("console")
                  .outputMode("append")
                  .start()
