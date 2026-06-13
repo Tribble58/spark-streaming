2667680cde8efef2dba4aa5d7d42e4f4dd6a2891
Vladlen <vladlensakhnyuk@yandex.ru>
2026-06-13 19:42:54 +0300

    Added stateful transformations - counting and summing and writing the results to console, changed logging level for spark and removed partition key for parquet

--- kafka_consumer_spark/main.py
+++ kafka_consumer_spark/main.py
@@ -1,14 +1,13 @@
 import logging
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, to_json, struct, to_date
+from pyspark.sql.functions import from_json, col, to_json, struct, to_date, sum, count
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 output_dir = "/data/spark_output"
-parquet_checkpoint_dir = "/tmp/parquet_checkpoints"
-kafka_checkpoint_dir = "/tmp/kafka_checkpoints"
+stateful_transformations_checkpoint_dir = "/tmp/stateful_transformations_checkpoint"
 
 if __name__ == '__main__':
     try:
@@ -16,6 +15,8 @@ if __name__ == '__main__':
                  .builder
                  .appName("tutorial")
                  .getOrCreate())
+
+        spark.sparkContext.setLogLevel("ERROR")
 
         df = spark \
             .readStream \
@@ -37,37 +38,18 @@ if __name__ == '__main__':
                                  col("timestamp").alias("processing_ts"),
                                  )
                          .select("json_data.event_id", "json_data.user_id", "json_data.action", "json_data.value",
-                                 col("json_data.event_time").alias("event_ts"), "offset", to_date(col("processing_ts")).alias("processing_date"))
+                                 col("json_data.event_time").alias("event_ts"), "offset",
+                                 to_date(col("processing_ts")).alias("processing_date"))
                          .filter(col("action") == "click")
                          .writeStream
                          .trigger(processingTime="5 seconds")
                          # Output result to parquet
                          .format("parquet")
                          .option("path", output_dir)
-                         .option("checkpointLocation", parquet_checkpoint_dir)
                          .partitionBy("processing_date")
                          .outputMode("append")
                          .start()
                          )
-
-        # query_console = (df
-        #                  .select(from_json(col("value").cast("string"),
-        #                                    kafka_message_schema)
-        #                          # required alias for further selecting
-        #                          .alias("json_data"),
-        #                          "offset",
-        #                          col("timestamp").alias("processing_ts"),
-        #                          )
-        #                  .select("json_data.event_id", "json_data.user_id", "json_data.action", "json_data.value",
-        #                          col("json_data.event_time").alias("event_ts"), "offset", "processing_ts")
-        #                  .filter(col("action") == "click")
-        #                  .writeStream
-        #                  .trigger(processingTime="5 seconds")
-        #                  # Output result to console
-        #                  .format("console")
-        #                  .outputMode("append")
-        #                  .start()
-        #                  )
 
         query_kafka = (df
                        .select(from_json(col("value").cast("string"),
@@ -94,10 +76,35 @@ if __name__ == '__main__':
                        .format("kafka")
                        .option("kafka.bootstrap.servers", "kafka:9092")
                        .option("topic", "stream-output")
-                       .option("checkpointLocation", kafka_checkpoint_dir)
                        .outputMode("append")
                        .start()
                        )
+
+        # Count number of clicks made by user
+        query_console = (df
+                         .select(from_json(col("value").cast("string"),
+                                           kafka_message_schema)
+                                 # required alias for further selecting
+                                 .alias("json_data"),
+                                 "offset",
+                                 col("timestamp").alias("processing_ts"),
+                                 )
+                         .select("json_data.user_id", "json_data.action", "json_data.value")
+                         .filter(col("action") == "click")
+                         .groupby("user_id")
+                         # .count()
+                         .agg(sum("value"),
+                              count("user_id").alias("count"))
+                         # Sorting is only available in complete mode
+                         .orderBy("count", ascending=False)
+                         .writeStream
+                         .trigger(processingTime="5 seconds")
+                         # Output result to console
+                         .format("console")
+                         .outputMode("complete")
+                         .option("checkpointLocation", stateful_transformations_checkpoint_dir)
+                         .start()
+                         )
 
         spark.streams.awaitAnyTermination()
 
