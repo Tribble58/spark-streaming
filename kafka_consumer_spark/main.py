bce1f0a109fdfd8f846fe03b356711cd5c1c94d4
Vladlen <vladlensakhnyuk@yandex.ru>
2026-06-15 20:34:32 +0300

    Added stateful transformations - counting and summing and writing the results to console, changed logging level for spark and removed partition key for parquet

--- kafka_consumer_spark/main.py
+++ kafka_consumer_spark/main.py
@@ -1,13 +1,12 @@
 import logging
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, to_json, struct, to_date, sum, count
+from pyspark.sql.functions import from_json, col, to_json, struct, to_date, sum, count, window
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 output_dir = "/data/spark_output"
-stateful_transformations_checkpoint_dir = "/tmp/stateful_transformations_checkpoint"
 
 if __name__ == '__main__':
     try:
@@ -17,6 +16,7 @@ if __name__ == '__main__':
                  .getOrCreate())
 
         spark.sparkContext.setLogLevel("ERROR")
+        spark.conf.set("spark.sql.streaming.checkpointLocation", "/tmp/checkpoints")
 
         df = spark \
             .readStream \
@@ -60,7 +60,8 @@ if __name__ == '__main__':
                                col("timestamp").alias("processing_ts"),
                                )
                        .select("json_data.event_id", "json_data.user_id", "json_data.action", "json_data.value",
-                               col("json_data.event_time").alias("event_ts"), "offset", "processing_ts")
+                               col("json_data.event_time").alias("event_ts"), "offset",
+                               to_date(col("processing_ts")).alias("processing_date"))
                        .filter(col("action") == "click")
                        .select(to_json
                                (struct
@@ -81,30 +82,62 @@ if __name__ == '__main__':
                        )
 
         # Count number of clicks made by user
-        query_console = (df
-                         .select(from_json(col("value").cast("string"),
-                                           kafka_message_schema)
-                                 # required alias for further selecting
-                                 .alias("json_data"),
-                                 "offset",
-                                 col("timestamp").alias("processing_ts"),
-                                 )
-                         .select("json_data.user_id", "json_data.action", "json_data.value")
-                         .filter(col("action") == "click")
-                         .groupby("user_id")
-                         # .count()
-                         .agg(sum("value"),
-                              count("user_id").alias("count"))
-                         # Sorting is only available in complete mode
-                         .orderBy("count", ascending=False)
-                         .writeStream
-                         .trigger(processingTime="5 seconds")
-                         # Output result to console
-                         .format("console")
-                         .outputMode("complete")
-                         .option("checkpointLocation", stateful_transformations_checkpoint_dir)
-                         .start()
-                         )
+        # query_console = (df
+        #                      .select(from_json(col("value").cast("string"),
+        #                                        kafka_message_schema)
+        #                              # required alias for further selecting
+        #                              .alias("json_data"),
+        #                              "offset",
+        #                              col("timestamp").alias("processing_ts"),
+        #                              )
+        #                      .select("json_data.event_id", "json_data.user_id", "json_data.action", "json_data.value",
+        #                              col("json_data.event_time").alias("event_ts"), "offset",
+        #                              to_date(col("processing_ts")).alias("processing_date"))
+        #                      .filter(col("action") == "click")
+        #                  .select("json_data.user_id", "json_data.action", "json_data.value")
+        #                  .filter(col("action") == "click")
+        #                  .groupby("user_id")
+        #                  # .count()
+        #                  .agg(sum("value"),
+        #                       count("user_id").alias("count"))
+        #                  # Sorting is only available in complete mode
+        #                  .orderBy("count", ascending=False)
+        #                  .writeStream
+        #                  .trigger(processingTime="5 seconds")
+        #                  # Output result to console
+        #                  .format("console")
+        #                  .outputMode("complete")
+        #                  .option("checkpointLocation", stateful_transformations_checkpoint_dir)
+        #                  .start()
+        #                  )
+
+        query_console_watermark = (
+            df
+            .select(from_json(col("value").cast("string"),
+                              kafka_message_schema)
+                    # required alias for further selecting
+                    .alias("json_data"),
+                    "offset",
+                    col("timestamp").alias("processing_ts"),
+                    )
+            .select("json_data.event_id", "json_data.user_id", "json_data.action", "json_data.value",
+                    col("json_data.event_time").alias("event_ts"), "offset",
+                    to_date(col("processing_ts")).alias("processing_date"))
+            .filter(col("action") == "click")
+            .select("user_id", "action", "value", "event_ts")
+            .withWatermark("event_ts", "10 seconds")
+            .groupBy(window("event_ts", "10 seconds", "5 seconds"))
+            .agg(
+                count("user_id").alias("count_in_window"),
+                sum("value").alias("sum_in_window")
+            )
+            .writeStream
+            # Output result to console
+            .format("console")
+            .outputMode("append")
+            .option("truncate", False)
+            .start()
+        )
 
         spark.streams.awaitAnyTermination()
 
